3789ee22274273b97e2aa6a1f08f927fbcc3bb4f
Vishwanath T S <vishu.vishwanath16@gmail.com>
2024-11-26 13:02:01 +0530

    m

--- Easy/dummy.py
+++ Easy/dummy.py
@@ -1,60 +1,13 @@
-class Node:
-    def __init__(self, data=None):
-        self.data = data
-        self.next = None
+def mergeAlternately(word1, word2):
+    result = ''
+    for i, j in zip(word1, word2):
+        result += i + j
+
+    result += word2[len(word1):]
+
+    
+
+    return result
 
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
-
-    def append(self, data):
-        new_node = Node(data)
-        if self.head is None:
-            self.head = new_node
-        else:
-            current = self.head
-            while current.next:
-                current = current.next
-            current.next = new_node
-
-    def prepend(self, data):
-
-        new_node = Node(data)
-        new_node.next = self.head
-        self.head = new_node
-
-    def delete(self, data):
-
-        current = self.head
-        if current and current.data == data:
-            self.head = current.next
-            current = None
-            return
-
-        prev = None
-        while current and current.data != data:
-            prev = current
-            current = current.next
-
-        if current is None:
-            return
-
-        prev.next = current.next
-        current = None
-
-    def display(self):
-        current = self.head
-        while current:
-            print(current.data, end=" -> ")
-            current = current.next
-        print("None")
-
-
-l1 = LinkedList()
-
-l1.append(1)
-l1.append(2)
-l1.append(3)
-
-l1.display()
+print(mergeAlternately("hello", "worldSheep"))
