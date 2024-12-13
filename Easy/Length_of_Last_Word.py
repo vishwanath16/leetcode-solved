64b7cc8afe67be23eac01cff5e24da86a4920d82
Vishwanath T S <vishu.vishwanath16@gmail.com>
2024-12-14 01:34:39 +0530

    m

--- Easy/Length_of_Last_Word.py
+++ Easy/Length_of_Last_Word.py
@@ -1,4 +1,5 @@
 # Link: https://leetcode.com/problems/length-of-last-word/description/
+# Solution: https://leetcode.com/problems/length-of-last-word/solutions/6144204/python-one-liner-solution-beats-100-runt-3adn
 
 def lengthOfLastWord(s):
     return s.strip().split(" ")[-1]
