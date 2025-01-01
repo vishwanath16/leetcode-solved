2bd5b2c9c514c00dc9ad3ee6689289faf2bd4839
Vishwanath T S <vishu.vishwanath16@gmail.com>
2025-01-01 21:50:08 +0530

    Implement isValid function to check for valid parentheses

--- Stack/Valid_Parentheses.py
+++ Stack/Valid_Parentheses.py
@@ -1,2 +1,21 @@
 # Link: https://leetcode.com/problems/valid-parentheses/description/
 
+
+def isValid(s):
+
+    sets = {')': '(', '}': '{', ']': '['}
+
+    stack = []
+
+    for c in s:
+        if c in sets:
+            if stack and stack[-1] == sets[c]:
+                stack.pop()
+            else:
+                return False 
+        else:
+            stack.append(c)
+    
+    return not stack
+
+print(isValid('()[]{}'))
