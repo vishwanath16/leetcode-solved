9b13189b10569402247c673147ca8e403e8b9359
Vishwanath T S <vishu.vishwanath16@gmail.com>
2025-01-02 00:39:02 +0530

    Add solutions for reversing a linked list and inverting a binary tree

---
+++ Trees/Invert_Binary_Tree.py
@@ -0,0 +1,39 @@
+# Link: https://leetcode.com/problems/invert-binary-tree/description/
+
+# Recursive Way:
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        
+        return root
+
+#Iterative Way:
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        stack = [root]
+
+        while stack:
+            node = stack.pop()
+            node.left, node.right = node.right, node.left
+
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+
+        return root
