12e836540bc5d51e3b1d50813cc9f064e7af3bce
Vishwanath T S <vishu.vishwanath16@gmail.com>
2025-01-26 15:10:00 +0530

    m

--- Arrays & Hashing/Top_K_Frequent_Elements.py
+++ Arrays & Hashing/Top_K_Frequent_Elements.py
@@ -1,2 +1,18 @@
 # Link: https://leetcode.com/problems/top-k-frequent-elements/description/
 
+from collections import Counter
+
+def topKFrequent(nums, k):
+    counts = Counter(nums)
+    
+    # Sort keys by frequency (counts) in descending order
+    sorted_keys = sorted(counts.keys(), key= lambda x : counts[x], reverse=True)
+    
+    print(sorted_keys)
+
+    # Return the top k keys
+    return sorted_keys[:k]
+
+
+print(topKFrequent([4,1,-1,2,-1,2,3], 2))
+
