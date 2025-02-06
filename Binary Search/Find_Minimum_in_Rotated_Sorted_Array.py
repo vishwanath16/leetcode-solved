61db11445b97e67f6dba29d89acc385c80c11c69
Vishwanath T S <vishu.vishwanath16@gmail.com>
2025-02-06 14:30:58 +0530

    m

--- Binary Search/Find_Minimum_in_Rotated_Sorted_Array.py
+++ Binary Search/Find_Minimum_in_Rotated_Sorted_Array.py
@@ -1,2 +1,17 @@
 # Link: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
 
+def findMin(nums):
+    left, right = 0, len(nums) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+
+        if(nums[mid] > nums[right]):
+            left = mid + 1
+        else:
+            right = mid
+
+    return nums[left]
+
+
+print(findMin([3,4,5,1,2])) #Rotated 3 times
